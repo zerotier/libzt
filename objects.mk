1f74ceaea68260bde373acf3440ff51f46c6d480
Joseph Henry <josephjah@gmail.com>
2016-06-15 10:16:26 -0700

    Rebuilt ZeroTierUnity-OSX bundle project

---
+++ objects.mk
@@ -0,0 +1,32 @@
+OBJS=\
+	ext/lz4/lz4.o \
+	ext/json-parser/json.o \
+	ext/http-parser/http_parser.o \
+	node/C25519.o \
+	node/CertificateOfMembership.o \
+	node/Cluster.o \
+	node/DeferredPackets.o \
+	node/Dictionary.o \
+	node/Identity.o \
+	node/IncomingPacket.o \
+	node/InetAddress.o \
+	node/Multicaster.o \
+	node/Network.o \
+	node/NetworkConfig.o \
+	node/Node.o \
+	node/OutboundMulticast.o \
+	node/Packet.o \
+	node/Path.o \
+	node/Peer.o \
+	node/Poly1305.o \
+	node/Salsa20.o \
+	node/SelfAwareness.o \
+	node/SHA512.o \
+	node/Switch.o \
+	node/Topology.o \
+	node/Utils.o \
+	osdep/BackgroundResolver.o \
+	osdep/Http.o \
+	osdep/OSUtils.o \
+	service/ClusterGeoIpService.o \
+	service/ControlPlane.o

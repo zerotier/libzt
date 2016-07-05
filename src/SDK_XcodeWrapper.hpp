075a53475fd8c36db3069137ac942106e355eb03
Joseph Henry <josephjah@gmail.com>
2016-07-05 14:47:14 -0500

    OSX SDK demo app update

---
+++ src/SDK_XcodeWrapper.hpp
@@ -0,0 +1,14 @@
+//
+//  SDK_XcodeWrapper.hpp
+//  Netcon-iOS
+//
+//  Created by Joseph Henry on 2/14/16.
+//  Copyright © 2016 ZeroTier. All rights reserved.
+//
+
+#ifndef SDK_XCODE_WRAPPER_HPP
+#define SDK_XCODE_WRAPPER_HPP
+
+#include <stdio.h>
+
+#endif /* SDK_XCODE_WRAPPER_HPP */

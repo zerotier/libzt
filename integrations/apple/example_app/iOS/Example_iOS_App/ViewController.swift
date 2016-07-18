30418eaef229c198d822824c72a24d847d4d9809
Joseph Henry <Joseph@Josephs-MacBook-Pro.local>
2016-07-17 17:16:18 -0700

    adjusted bridging-header reference for ios/osx xcode projects

--- integrations/apple/example_app/iOS/Example_iOS_App/ViewController.swift
+++ integrations/apple/example_app/iOS/Example_iOS_App/ViewController.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var service_thread : NSThread!
+    func ztnc_start_service() {
+        let path = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        print("start_service()\n")
+        // e5cd7a9e1c3511dd
+        start_service("/Users/Joseph/utest3")
+        //start_service(path[0])
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // ZeroTier Service thread
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), {
+            self.service_thread = NSThread(target:self, selector:"ztnc_start_service", object:nil)
+            self.service_thread.start()
+        });
+        
+        // Set RPC path for this thread
+        zts_init_rpc("/Users/Joseph/utest3/nc_","e5cd7a9e1c2e194f");
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 

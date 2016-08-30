8eb0acf79c6918a4d8e446c662e5f56a982246b0
Joseph Henry <josephjah@gmail.com>
2016-08-30 16:54:06 -0700

    updated makefiles

--- make-mac.mk
+++ make-mac.mk
@@ -49,24 +49,53 @@ endif
 
 CXXFLAGS=$(CFLAGS) -fno-rtti
 
-# Build everything
-all: osx ios android lwip check
 
+# Target output filenames
+SHARED_LIB_NAME    = libztosx.so
+INTERCEPT_NAME     = libztintercept.so
+SDK_SERVICE_NAME   = zerotier-sdk-service
+ONE_SERVICE_NAME   = zerotier-one
+ONE_CLI_NAME       = zerotier-cli
+ONE_ID_TOOL_NAME   = zerotier-idtool
+LWIP_LIB_NAME      = liblwip.so
+#
+SHARED_LIB         = $(BUILD)/$(SHARED_LIB_NAME)
+INTERCEPT          = $(BUILD)/$(INTERCEPT_NAME)
+SDK_SERVICE        = $(BUILD)/$(SDK_SERVICE_NAME)
+ONE_SERVICE        = $(BUILD)/$(ONE_SERVICE_NAME)
+ONE_CLI            = $(BUILD)/$(ONE_CLI_NAME)
+ONE_IDTOOL         = $(BUILD)/$(ONE_IDTOOL_NAME)
+LWIP_LIB           = $(BUILD)/$(LWIP_LIB_NAME)
+
+
+# Build everything
+all: one osx ios android lwip check
+
+
+
+
+# --- EXTERNAL LIBRARIES ---
 lwip:
 	make -f make-liblwip.mk
 
+
+
+# ------- IOS / OSX --------
 # Build all Apple targets
-apple: osx ios
+apple: one osx ios
+
+# Build vanilla ZeroTier One binary
+one: $(OBJS) $(ZT1)/service/OneService.o $(ZT1)/one.o $(ZT1)/osdep/OSXEthernetTap.o
+	mkdir -p $(BUILD)
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $(BUILD)/zerotier-one $(OBJS) $(ZT1)/service/OneService.o $(ZT1)/one.o $(ZT1)/osdep/OSXEthernetTap.o $(LDLIBS)
+	$(STRIP) $(ONE_SERVICE)
+	cp $(ONE_SERVICE) $(INT)/docker/docker_demo/$(ONE_SERVICE_NAME)
 
 # Build all iOS targets
 ios: ios_app_framework ios_unity3d_bundle
 
 # Build all OSX targets
 osx: osx_app_framework osx_unity3d_bundle osx_shared_lib osx_service_and_intercept
-
-# Build all Android targets 
-# Chip architectures can be specified in integrations/android/android_jni_lib/java/jni/Application.mk
-android: android_jni_lib
 
 # TODO: CHECK if XCODE TOOLS are installed
 # Build frameworks for application development
@@ -91,6 +120,40 @@ ios_unity3d_bundle:
 	cd $(INT)/apple/ZeroTierSDK_Apple; xcodebuild -configuration Release -scheme ZeroTierSDK_Unity3D_iOS build SYMROOT="../../../$(BUILD)/ios_unity3d_bundle"
 	cd $(INT)/apple/ZeroTierSDK_Apple; xcodebuild -configuration Debug -scheme ZeroTierSDK_Unity3D_iOS build SYMROOT="../../../$(BUILD)/ios_unity3d_bundle"
 	cp docs/ios_unity3d_zt_sdk.md $(BUILD)/ios_unity3d_bundle/README.md
+# 
+simple_app: tests osx_service_and_intercept
+	mkdir -p build/simple_app
+	mkdir -p build/tests/zerotier
+	$(CXX) -Isrc $(SHARED_LIB) integrations/simple_app/app.cpp -o $(BUILD)/simple_app/app
+	cp $(LWIP_LIB) $(BUILD)/tests/zerotier/$(LWIP_LIB_NAME)
+
+remove_only_intermediates:
+	-find . -type f \( -name '*.o' -o -name '*.so' \) -delete
+
+# Builds a single shared library which contains everything
+osx_shared_lib: remove_only_intermediates $(OBJS)
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -DSDK -DZT_ONE_NO_ROOT_CHECK -Iext/lwip/src/include -Iext/lwip/src/include/ipv4 -Iext/lwip/src/include/ipv6 -Izerotierone/osdep -Izerotierone/node -Izerotierone/service -Isrc -shared -o $(SHARED_LIB) $(OBJS) zerotierone/service/OneService.cpp src/SDK_Service.cpp src/SDK_EthernetTap.cpp src/SDK_Proxy.cpp zerotierone/one.cpp -x c src/SDK_Sockets.c src/SDK_Intercept.c src/SDK_Debug.c src/SDK_RPC.c $(LDLIBS) -ldl
+
+osx_intercept:
+	# Use gcc not clang to build standalone intercept library since gcc is typically used for libc and we want to ensure maximal ABI compatibility
+	cd src ; gcc $(DEFS) -O2 -Wall -std=c99 -fPIC -fno-common -dynamiclib -flat_namespace -DVERBOSE -D_GNU_SOURCE -DNETCON_INTERCEPT -I. -I../zerotierone/node -nostdlib -shared -o ../$(INTERCEPT) SDK_Sockets.c SDK_Intercept.c SDK_Debug.c SDK_RPC.c -ldl
+	#mv src/$(INTERCEPT_NAME) $(INTERCEPT)
+
+# Build only the SDK service
+osx_sdk_service: lwip $(OBJS)
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -DSDK -DZT_ONE_NO_ROOT_CHECK -Iext/lwip/src/include -Iext/lwip/src/include/ipv4 -Iext/lwip/src/include/ipv6 -Izerotierone/osdep -Izerotierone/node -Isrc -o $(BUILD)/zerotier-sdk-service $(OBJS) zerotierone/service/OneService.cpp src/SDK_EthernetTap.cpp src/SDK_Proxy.cpp zerotierone/one.cpp -x c src/SDK_RPC.c $(LDLIBS) -ldl
+	ln -sf $(SDK_SERVICE_NAME) zerotier-cli
+	ln -sf $(SDK_SERVICE_NAME) zerotier-idtool
+
+# Build both intercept library and SDK service (separate)
+osx_service_and_intercept: remove_only_intermediates osx_intercept osx_sdk_service $(OBJS)
+
+
+
+# -------- ANDROID ---------
+# Build all Android targets 
+# Chip architectures can be specified in integrations/android/android_jni_lib/java/jni/Application.mk
+android: android_jni_lib
 
 # TODO: CHECK if ANDROID/GRADLE TOOLS are installed
 # Build library for Android Unity integrations
@@ -103,38 +166,17 @@ android_jni_lib:
 	mv -f $(INT)/android/android_jni_lib/java/libs/* $(BUILD)/android_jni_lib
 	cp -R $(BUILD)/android_jni_lib/* $(INT)/android/example_app/app/src/main/jniLibs
 
-# 
-simple_app: tests osx_service_and_intercept
-	mkdir -p build/simple_app
-	mkdir -p build/tests/zerotier
-	$(CXX) -Isrc $(BUILD)/libztosx.so integrations/simple_app/app.cpp -o $(BUILD)/simple_app/app
-	cp $(BUILD)/lwip/liblwip.so $(BUILD)/tests/zerotier/liblwip.so
 
-remove_only_intermediates:
-	-find . -type f \( -name '*.o' -o -name '*.so' \) -delete
 
-osx_shared_lib: remove_only_intermediates $(OBJS)
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -DSDK -DZT_ONE_NO_ROOT_CHECK -Iext/lwip/src/include -Iext/lwip/src/include/ipv4 -Iext/lwip/src/include/ipv6 -Izerotierone/osdep -Izerotierone/node -Izerotierone/service -Isrc -shared -o $(BUILD)/libztosx.so $(OBJS) zerotierone/service/OneService.cpp src/SDK_Service.cpp src/SDK_EthernetTap.cpp src/SDK_Proxy.cpp zerotierone/one.cpp -x c src/SDK_Sockets.c src/SDK_Intercept.c src/SDK_Debug.c src/SDK_RPC.c $(LDLIBS) -ldl
 
-osx_service_and_intercept: remove_only_intermediates $(OBJS)
-	# Need to selectively rebuild one.cpp and OneService.cpp with ZT_SERVICE_NETCON and ZT_ONE_NO_ROOT_CHECK defined, and also NetconEthernetTap
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -DSDK -DZT_ONE_NO_ROOT_CHECK -Iext/lwip/src/include -Iext/lwip/src/include/ipv4 -Iext/lwip/src/include/ipv6 -Izerotierone/osdep -Izerotierone/node -Isrc -o $(BUILD)/zerotier-sdk-service $(OBJS) zerotierone/service/OneService.cpp src/SDK_EthernetTap.cpp src/SDK_Proxy.cpp zerotierone/one.cpp -x c src/SDK_RPC.c $(LDLIBS) -ldl
-	# Build liblwip.so which must be placed in ZT home for zerotier-sdk-service to work
-	make -f make-liblwip.mk
-	# Use gcc not clang to build standalone intercept library since gcc is typically used for libc and we want to ensure maximal ABI compatibility
-	cd src ; gcc $(DEFS) -O2 -Wall -std=c99 -fPIC -fno-common -dynamiclib -flat_namespace -DVERBOSE -D_GNU_SOURCE -DNETCON_INTERCEPT -I. -I../zerotierone/node -nostdlib -shared -o libztintercept.so SDK_Sockets.c SDK_Intercept.c SDK_Debug.c SDK_RPC.c -ldl
-	mv src/libztintercept.so $(BUILD)/libztintercept.so
-	ln -sf zerotier-sdk-service zerotier-cli
-	ln -sf zerotier-sdk-service zerotier-idtool
-
-prep:
-	cp $(INT)/android/android_jni_lib/java/libs/* build
-
+# -------- TESTING ---------
 # Check for the presence of built frameworks/bundles/libaries
 check:
-	./check.sh $(BUILD)/lwip/liblwip.so
-	./check.sh $(BUILD)/libztintercept.so
-	./check.sh $(BUILD)/libztosx.so
+	./check.sh $(LWIP_LIB)
+	./check.sh $(INTERCEPT)
+	./check.sh $(ONE_SERVICE)
+	./check.sh $(SDK_SERVICE)
+	./check.sh $(SHARED_LIB)
 	./check.sh $(BUILD)/osx_unity3d_bundle/Debug/ZeroTierSDK_Unity3D_OSX.bundle
 	./check.sh $(BUILD)/osx_app_framework/Debug/ZeroTierSDK_OSX.framework
 	./check.sh $(BUILD)/ios_app_framework/Debug-iphoneos/ZeroTierSDK_iOS.framework
@@ -165,15 +207,18 @@ tests: $(TEST_OBJDIR) $(TEST_TARGETS) osx_service_and_intercept
 	cp tests/api_test/servers.sh $(BUILD)/tests/servers.sh
 	cp tests/api_test/clients.sh $(BUILD)/tests/clients.sh
 	cp tests/cleanup.sh $(BUILD)/tests/cleanup.sh
-	cp $(BUILD)/lwip/liblwip.so $(BUILD)/tests/zerotier/liblwip.so
+	cp $(LWIP_LIB) $(BUILD)/tests/zerotier/$(LWIP_LIB_NAME)
 
+
+
+# ----- ADMINISTRATIVE -----
 JAVAC := $(shell which javac)
 
 clean_unity:
 	
 clean_android:
 	# android JNI lib project
-	test -s /usr/bin/javac || { echo "Javac not found"; exit 1; }
+	-test -s /usr/bin/javac || { echo "Javac not found"; exit 1; }
 	-cd $(INT)/android/android_jni_lib/proj; ./gradlew clean
 	-rm -rf $(INT)/android/android_jni_lib/proj/build
 	# example android app project
@@ -183,7 +228,7 @@ clean_basic:
 	-rm -rf $(BUILD)/*
 	-rm -rf $(INT)/Unity3D/Assets/Plugins/*
 	-rm -rf zerotier-cli zerotier-idtool
-	-find . -type f \( -name 'zerotier-one' -o -name 'zerotier-sdk-service' \) -delete
+	-find . -type f \( -name $(ONE_SERVICE_NAME) -o -name $(SDK_SERVICE_NAME) \) -delete
 	-find . -type f \( -name '*.o' -o -name '*.so' -o -name '*.o.d' -o -name '*.out' -o -name '*.log' -o -name '*.dSYM' \) -delete
 
 clean: clean_basic clean_android
@@ -191,7 +236,8 @@ clean: clean_basic clean_android
 clean_for_production:
 	-find . -type f \( -name '*.identity'\) -delete
 
-## --- BELOW ARE DEVELOPMENT AIDS ---
+prep:
+	cp $(INT)/android/android_jni_lib/java/libs/* build
 
 # Copy and rename source files into example projects to follow local ordinances
 update_examples:

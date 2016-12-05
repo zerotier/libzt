9a2092acc5ca5b7f081ed12325dc5121292c95cb
Joseph Henry <josephjah@gmail.com>
2016-12-05 10:32:35 -0800

    xcode osx example app update

--- integrations/apple/example_app/OSX/Example_OSX_App/AppDelegate.swift
+++ integrations/apple/example_app/OSX/Example_OSX_App/AppDelegate.swift
@@ -11,8 +11,6 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }

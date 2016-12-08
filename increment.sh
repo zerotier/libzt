a4aeaee602d3beb90ff8a7de4f4d5699da2d4269
Joseph Henry <josephjah@gmail.com>
2016-12-08 10:13:34 -0800

    removed unused files

--- increment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BUILD_NUMBER_STR=$(cat .buildnum)
-BUILD_NUMBER=$((BUILD_NUMBER_STR + 1))
-echo $BUILD_NUMBER > .buildnum
-echo "#define ZTSDK_BUILD_VERSION " $BUILD_NUMBER > src/build.h 
-echo $BUILD_NUMBER 

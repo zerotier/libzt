f13f06ae7092ca8b2b3984b870a9077a1507af15
Joseph Henry <josephjah@gmail.com>
2017-06-26 02:46:59 -0700

    Added ZeroTierOne as a submodule

--- zto/ext/installfiles/mac/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-zthome="/Library/Application Support/ZeroTier/One"
-export PATH="$zthome:/bin:/usr/bin:/sbin:/usr/sbin"
-
-# Launch ZeroTier One (not as daemon... launchd monitors it)
-exec zerotier-one

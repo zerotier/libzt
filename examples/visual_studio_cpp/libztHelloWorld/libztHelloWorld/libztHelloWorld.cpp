8f0405abc95bf5c27966aa6341ea3ce9dcfd2260
Joseph Henry <josephjah@gmail.com>
2017-10-11 15:22:57 -0700

    Added example Visual Studio C++ project

---
+++ examples/visual_studio_cpp/libztHelloWorld/libztHelloWorld/libztHelloWorld.cpp
@@ -0,0 +1,22 @@
+// libztHelloWorld - Simple demo with libzt.lib and libzt.dll
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <stdio.h>
+#include <windows.h>
+#include <strsafe.h>
+
+#include "C:\msys\home\ZeroTier\libzt\include\libzt.h"
+
+int main()
+{
+	printf("waiting for libzt to come online...\n");
+	zts_simple_start("dlldir", "17d709436c2c5367");
+	printf("started. now performing a socket call\n");
+	int fd = zts_socket(AF_INET, SOCK_STREAM, 0);
+	printf("fd=%d\n", fd);
+	// zts_connect(), zts_bind(), etc...
+	zts_stop();
+  return 0;
+}
+

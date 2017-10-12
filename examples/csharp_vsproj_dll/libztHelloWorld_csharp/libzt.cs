2b417bb7bd9efabf38fda23cdb0068db10f38d4e
Joseph Henry <josephjah@gmail.com>
2017-10-12 13:26:52 -0700

    Added minimal example csharp (C#) visual studio project

---
+++ examples/csharp_vsproj_dll/libztHelloWorld_csharp/libzt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+
+namespace ZeroTier
+{
+    static class libzt
+    {
+        [DllImport("libzt.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int zts_startjoin(string path, string nwid);
+
+        [DllImport("libzt.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int zts_socket(int socket_family, int socket_type, int protocol);
+
+        [DllImport("libzt.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int zts_stop();
+    }
+}

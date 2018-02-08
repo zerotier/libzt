c00f2a97e6f9ffa24ad42e694992edd26227304f
Joseph Henry <josephjah@gmail.com>
2018-02-08 11:10:48 -0800

    Cleanup to test.py

--- packages/pypi/test.py
+++ packages/pypi/test.py
@@ -1,10 +1,23 @@
 import libzt
 import time
 
-print('joining virtual network...')
-libzt.zts_startjoin('whatev_config', 0x1234567890ABCDEF)
-print('fd = ' + str(libzt.zts_socket(1,2,3)))
+nwid = 0x1234567890ABCDEF
 
-print('looping forever, ping me')
+# Authorization required via my.zerotier.com if this is a private network).
+print('Joining virtual network...')
+libzt.zts_startjoin('whatev_config', nwid)
+
+fd = libzt.zts_socket(AF_INET,SOCK_STREAM,0)
+if fd < 0:
+	print('error creating socket')
+print('fd = ' + str(fd))
+
+# Display info about this node
+print('I am ' + str(hex(libzt.zts_get_node_id())))
+address = None
+libzt.zts_get_address(nwid, address, AF_INET)
+print('assigned address = ' + str(address))
+
+print('Looping forever, ping me or something')
 while True:
     time.sleep(1)

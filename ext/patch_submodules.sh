6e3e14fdff4e434c72889c7893121c602e2b70ff
Joseph Henry <josephjah@gmail.com>
2018-07-27 15:49:58 -0700

    Added patch for lwip-contrib for Windows compatibility

--- ext/patch_submodules.sh
+++ ext/patch_submodules.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Execute from project root directory
+
 cd ext/lwip
 cp ../lwip.patch .
 git apply lwip.patch
-cd -
+cd ../..
+
+cd ext/lwip-contrib
+cp ../lwip-contrib.patch .
+git apply lwip-contrib.patch
+
+cd ../..

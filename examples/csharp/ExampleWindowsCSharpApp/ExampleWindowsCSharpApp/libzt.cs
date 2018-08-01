1b4cbb32e049d3531551c065fcf272ce12b22d51
Joseph Henry <josephjah@gmail.com>
2018-08-01 00:11:38 -0700

    Added more example C# DLL client/server code

--- examples/csharp/ExampleWindowsCSharpApp/ExampleWindowsCSharpApp/libzt.cs
+++ examples/csharp/ExampleWindowsCSharpApp/ExampleWindowsCSharpApp/libzt.cs
@@ -10,6 +10,42 @@ namespace ZeroTier
 {
     static class libzt
     {
+        public enum SockAddrFamily
+        {
+            Inet = 2,
+            Inet6 = 10
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SockAddr
+        {
+            public ushort Family;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 14)]
+            public byte[] Data;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SockAddrIn
+        {
+            byte len; // unique to lwIP
+            public byte Family;
+            public Int16 Port;
+            public uint Addr;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public byte[] Zero;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SockAddrIn6
+        {
+            public ushort Family;
+            public ushort Port;
+            public uint FlowInfo;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+            public byte[] Addr;
+            public uint ScopeId;
+        };
+
         /****************************************************************************/
         /* ZeroTier Service Controls                                                */
         /****************************************************************************/
@@ -33,7 +69,7 @@ namespace ZeroTier
         public static extern void zts_get_homepath(string homePath, int len);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_get_id(string nodeId);
+        public static extern Int64 zts_get_node_id();
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int zts_running();
@@ -42,13 +78,13 @@ namespace ZeroTier
         public static extern int zts_has_address(ulong nwid);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void zts_get_address(ulong nwid, System.IntPtr addr, int addrlen);
+        public static extern void zts_get_address(ulong nwid, IntPtr addr, int addrlen);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void zts_get_6plane_addr(System.IntPtr addr, string nwid, string nodeId);
+        public static extern void zts_get_6plane_addr(IntPtr addr, string nwid, string nodeId);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void zts_get_rfc4193_addr(System.IntPtr addr, string nwid, string nodeId);
+        public static extern void zts_get_rfc4193_addr(IntPtr addr, string nwid, string nodeId);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern long zts_get_peer_count();
@@ -64,16 +100,16 @@ namespace ZeroTier
         public static extern int zts_socket(int socket_family, int socket_type, int protocol);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_connect(int fd, System.IntPtr addr, int addrlen);
+        public static extern int zts_connect(int fd, IntPtr addr, int addrlen);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_bind(int fd, System.IntPtr addr, int addrlen);
+        public static extern int zts_bind(int fd, IntPtr addr, int addrlen);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int zts_listen(int fd, int backlog);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_accept(int fd, System.IntPtr addr, IntPtr addrlen);
+        public static extern int zts_accept(int fd, IntPtr addr, IntPtr addrlen);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int zts_setsockopt(int fd, int level, int optname, IntPtr optval, int optlen);
@@ -82,7 +118,7 @@ namespace ZeroTier
         public static extern int zts_getsockopt(int fd, int level, int optname, IntPtr optval, IntPtr optlen);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_getsockname(int fd, System.IntPtr addr, IntPtr addrlen);
+        public static extern int zts_getsockname(int fd, IntPtr addr, IntPtr addrlen);
 
         /*
                 [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
@@ -111,28 +147,28 @@ namespace ZeroTier
         public static extern int zts_ioctl(int fd, ulong request, IntPtr argp);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_send(int fd, IntPtr buf, int len, int flags);
+        public static extern int zts_send(int fd, byte[] buf, int len, int flags);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_sendto(int fd, IntPtr buf, int len, int flags, System.IntPtr addr, int addrlen);
+        public static extern int zts_sendto(int fd, byte[] buf, int len, int flags, IntPtr addr, int addrlen);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_sendmsg(int fd, IntPtr msg, int flags);
+        public static extern int zts_sendmsg(int fd, byte[] msg, int flags);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_recv(int fd, IntPtr buf, int len, int flags);
+        public static extern int zts_recv(int fd, byte[] buf, int len, int flags);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_recvfrom(int fd, IntPtr buf, int len, int flags, System.IntPtr addr, IntPtr addrlen);
+        public static extern int zts_recvfrom(int fd, byte[] buf, int len, int flags, IntPtr addr, IntPtr addrlen);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_recvmsg(int fd, IntPtr msg, int flags);
+        public static extern int zts_recvmsg(int fd, byte[] msg, int flags);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_read(int fd, IntPtr buf, int len);
+        public static extern int zts_read(int fd, byte[] buf, int len);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int zts_write(int fd, IntPtr buf, int len);
+        public static extern int zts_write(int fd, byte[] buf, int len);
 
         [DllImport("libzt.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int zts_shutdown(int fd, int how);

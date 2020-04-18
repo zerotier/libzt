8e770b5355c3b37b328c36b4499909dda3dad5b8
Joseph Henry <josephjah@gmail.com>
2020-04-17 19:35:46 -0700

    Update JNI error codes to match new scheme

--- src/java/ZeroTier.java
+++ src/java/ZeroTier.java
@@ -1,28 +1,15 @@
 /*
- * ZeroTier SDK - Network Virtualization Everywhere
- * Copyright (C) 2011-2019  ZeroTier, Inc.  https://www.zerotier.com/
+ * Copyright (c)2013-2020 ZeroTier, Inc.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file in the project's root directory.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Change Date: 2024-01-01
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * --
- *
- * You can be released from the requirements of the license by purchasing
- * a commercial license. Buying such a license is mandatory as soon as you
- * develop commercial closed-source software that incorporates or links
- * directly against ZeroTier software without disclosing the source code
- * of your own application.
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2.0 of the Apache License.
  */
+/****/
 
 package com.zerotier.libzt;
 
@@ -37,7 +24,7 @@ public class ZeroTier
 	// Everything is ok
 	public static int ZTS_ERR_OK                       =  0;
 	// Error
-	public static int ZTS_ERR_OK                       = -1;
+	public static int ZTS_ERR                          = -1;
 	// A argument provided by the user application is invalid (e.g. out of range, NULL, etc)
 	public static int ZTS_ERR_INVALID_ARG              = -2;
 	// The service isn't initialized or is for some reason currently unavailable. Try again.

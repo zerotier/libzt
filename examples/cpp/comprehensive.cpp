2467d6b071fa30fd71263f61c54ebf476cb7ed0a
Ben <63513959+Ben-L-E@users.noreply.github.com>
2021-04-14 12:35:24 -0700

    Gradle updates and various fixes

--- examples/cpp/comprehensive.cpp
+++ examples/cpp/comprehensive.cpp
@@ -89,6 +89,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <arpa/inet.h>
 
 #include "ZeroTierSockets.h"
 
--- examples/cpp/nonblockingserver.cpp
+++ examples/cpp/nonblockingserver.cpp
@@ -87,6 +87,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #include "ZeroTierSockets.h"
 

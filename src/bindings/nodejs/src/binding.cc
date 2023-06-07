150f2cf5d1649955bc444636b70cc0072f2b624b
janvanbouwel <mrpetmans@gmail.com>
2023-06-07 03:03:31 +0200

    Rename functions to match ZeroTierSockets.h implement cross-thread callback for events (only event code for now)

--- src/bindings/nodejs/src/binding.cc
+++ src/bindings/nodejs/src/binding.cc
@@ -2,56 +2,94 @@
 
 #include <napi.h>
 
-Napi::Value InitFromStorage(const Napi::CallbackInfo& info)
+using namespace Napi;
+
+Value init_from_storage(const CallbackInfo& info)
 {
-    Napi::Env env = info.Env();
+    Env env = info.Env();
 
     if (info.Length() < 1) {
-        Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+        TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
         return env.Null();
     }
     auto configPath = std::string(info[0].ToString()).c_str();
 
     int err = ZTS_ERR_OK;
     if ((err = zts_init_from_storage(configPath)) != ZTS_ERR_OK) {
-        Napi::Error::New(env, "Unable to set config path.").ThrowAsJavaScriptException();
+        Error::New(env, "Unable to set config path.").ThrowAsJavaScriptException();
         return env.Null();
     }
 
     return env.Null();
 }
 
-void on_zts_event(void* msgPtr)
-{
-    zts_event_msg_t* msg = (zts_event_msg_t*)msgPtr;
+ThreadSafeFunction event_callback;
 
-    printf("ZTS EVENT %d\n", msg->event_code);
+void event_handler(void* msgPtr)
+{
+    event_callback.Acquire();
+
+    zts_event_msg_t *msg = (zts_event_msg_t *)msgPtr;
+    double event = msg->event_code;
+    auto cb = [=]( Env env, Function jsCallback ) {
+        jsCallback.Call({Number::New( env, event)});
+    };
+
+    event_callback.NonBlockingCall(cb);
+
+    event_callback.Release();
 }
 
-Napi::Value StartNode(const Napi::CallbackInfo& info)
+Value init_set_event_handler(const CallbackInfo& info)
 {
-    Napi::Env env = info.Env();
+    Env env = info.Env();
+
+    if (info.Length() < 1) {
+        TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    if (! info[0].IsFunction()) {
+        TypeError::New(env, "Callback should be a function").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    auto cb = info[0].As<Function>();
+    event_callback = ThreadSafeFunction::New(
+        env,
+        cb,
+        "zts_event_listener",
+        0,
+        1
+    );
+
+    int err = ZTS_ERR_OK;
+    if ((err = zts_init_set_event_handler(&event_handler)) != ZTS_ERR_OK) {
+        Error::New(env, "Unable to start service.").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    return env.Null();
+}
+
+Value start_node(const CallbackInfo& info)
+{
+    Env env = info.Env();
 
     int err = ZTS_ERR_OK;
 
-    if ((err = zts_init_set_event_handler(&on_zts_event)) != ZTS_ERR_OK) {
-        Napi::Error::New(env, "Unable to start service.").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
     if ((err = zts_node_start()) != ZTS_ERR_OK) {
-        Napi::Error::New(env, "Unable to start service.").ThrowAsJavaScriptException();
+        Error::New(env, "Unable to start service.").ThrowAsJavaScriptException();
         return env.Null();
-    }
-
-    while (! zts_node_is_online()) {
-        zts_util_delay(50);
     }
 
     return env.Null();
 }
 
-Napi::Value JoinNetwork(const Napi::CallbackInfo& info)
+Value node_is_online(const CallbackInfo& info)
+{
+    return Boolean::New(info.Env(), zts_node_is_online());
+}
+
+Value util_delay(const CallbackInfo& info)
 {
     Napi::Env env = info.Env();
 
@@ -59,89 +97,103 @@ Napi::Value JoinNetwork(const Napi::CallbackInfo& info)
         Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
         return env.Null();
     }
+    if (! info[0].IsNumber()) {
+        Napi::TypeError::New(env, "Wrong argument").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    auto delay = info[0].As<Napi::Number>().Uint32Value();
+
+    zts_util_delay(delay);
+
+    return env.Null();
+}
+
+Value net_join(const CallbackInfo& info)
+{
+    Env env = info.Env();
+
+    if (info.Length() < 1) {
+        TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+        return env.Null();
+    }
 
     if (! info[0].IsBigInt()) {
-        Napi::TypeError::New(env, "Network should be a bigint").ThrowAsJavaScriptException();
+        TypeError::New(env, "Network Id should be a bigint").ThrowAsJavaScriptException();
         return env.Null();
     }
 
     bool lossless = true;
-    auto net_id = info[0].As<Napi::BigInt>().Uint64Value(&lossless);
-    
+    auto net_id = info[0].As<BigInt>().Uint64Value(&lossless);
+
     int err;
     if ((err = zts_net_join(net_id)) != ZTS_ERR_OK) {
-        Napi::Error::New(env, "Unable to join network.").ThrowAsJavaScriptException();
+        Error::New(env, "Unable to join network.").ThrowAsJavaScriptException();
         return env.Null();
-    }
-
-    while(!  zts_net_transport_is_ready(net_id)) {
-        zts_util_delay(50);
     }
 
     return env.Null();
 }
 
-Napi::Value GetNetworkIPv4Addr(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
-
-    if (info.Length() < 1) {
-        Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    if (! info[0].IsBigInt()) {
-        Napi::TypeError::New(env, "Network should be a bigint").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    bool lossless;
-    auto net_id = info[0].As<Napi::BigInt>().Uint64Value(&lossless);
-
-    char addr[ZTS_IP_MAX_STR_LEN];
-    
-    int err;
-    if ((err = zts_addr_get_str(net_id, ZTS_AF_INET, addr, ZTS_IP_MAX_STR_LEN)) != ZTS_ERR_OK) {
-        Napi::Error::New(env, "Unable to get ipv4 address.").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    return Napi::String::New(env, addr);
-}
-
-Napi::Value GetNetworkIPv6Addr(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
-
-    if (info.Length() < 1) {
-        Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    if (! info[0].IsBigInt()) {
-        Napi::TypeError::New(env, "Network should be a bigint").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    bool lossless;
-    auto net_id = info[0].As<Napi::BigInt>().Uint64Value(&lossless);
-
-    char addr[ZTS_IP_MAX_STR_LEN];
-    
-    int err;
-    if ((err = zts_addr_get_str(net_id, ZTS_AF_INET6, addr, ZTS_IP_MAX_STR_LEN)) != ZTS_ERR_OK) {
-        Napi::Error::New(env, "Unable to get ipv6 address.").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    return Napi::String::New(env, addr);
-}
-
-Napi::Object Init(Napi::Env env, Napi::Object exports)
+Value net_transport_is_ready(const CallbackInfo& info)
 {
-    exports.Set(Napi::String::New(env, "initFromStorage"), Napi::Function::New(env, InitFromStorage));
-    exports.Set(Napi::String::New(env, "startNode"), Napi::Function::New(env, StartNode));
-    exports.Set(Napi::String::New(env, "joinNetwork"), Napi::Function::New(env, JoinNetwork));
-    exports.Set(Napi::String::New(env, "getNetworkIPv4Addr"), Napi::Function::New(env, GetNetworkIPv4Addr));
-    exports.Set(Napi::String::New(env, "getNetworkIPv6Addr"), Napi::Function::New(env, GetNetworkIPv6Addr));
+    Env env = info.Env();
+
+    if (info.Length() < 1) {
+        TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    if (! info[0].IsBigInt()) {
+        TypeError::New(env, "Network Id should be a bigint").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    bool lossless = true;
+    auto net_id = info[0].As<BigInt>().Uint64Value(&lossless);
+
+    return Boolean::New(env, zts_net_transport_is_ready(net_id));
+}
+
+Value addr_get_str(const CallbackInfo& info)
+{
+    Env env = info.Env();
+
+    if (info.Length() < 2) {
+        TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    if (! info[0].IsBigInt()) {
+        TypeError::New(env, "Network should be a bigint.").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    auto family = info[1].ToBoolean() ? ZTS_AF_INET6 : ZTS_AF_INET;
+
+    bool lossless;
+    auto net_id = info[0].As<BigInt>().Uint64Value(&lossless);
+
+    char addr[ZTS_IP_MAX_STR_LEN];
+
+    int err;
+    if ((err = zts_addr_get_str(net_id, family, addr, ZTS_IP_MAX_STR_LEN)) != ZTS_ERR_OK) {
+        Error::New(env, "Unable to get ip address.").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    return String::New(env, addr);
+}
+
+Object Init(Env env, Object exports)
+{
+    exports.Set("init_from_storage", Function::New(env, init_from_storage));
+    exports.Set("init_set_event_handler", Function::New(env, init_set_event_handler));
+    exports.Set("start_node", Function::New(env, start_node));
+    exports.Set("node_is_online", Function::New(env, node_is_online));
+    exports.Set("util_delay", Function::New(env, util_delay));
+    exports.Set("net_join", Function::New(env, net_join));
+    exports.Set("net_transport_is_ready", Function::New(env, net_transport_is_ready));
+    exports.Set("addr_get_str", Function::New(env, addr_get_str));
     return exports;
 }
 
--- src/bindings/nodejs/test.js
+++ src/bindings/nodejs/test.js
@@ -1,24 +1,34 @@
 var zts = require('bindings')('node-libzt');
 
 console.log(zts);
-console.log(zts.initFromStorage('id')); // 'world'
+console.log(zts.init_from_storage('id')); // 'world'
 
-zts.startNode();
+zts.init_set_event_handler((ev) => console.log(ev));
+
+zts.start_node();
+
+while(! zts.node_is_online()) {
+    zts.util_delay(50);
+}
 
 console.log("node started?")
 
-// const nwid = Buffer.from("ff0000ffff0000000", "hex").readBigUInt64BE()
-const nwid = Buffer.from("c7c8172af1579292", "hex").readBigUInt64BE()
+const nwid = Buffer.from("ff0000ffff0000000", "hex").readBigUInt64BE()
 
-zts.joinNetwork(nwid)
+
+zts.net_join(nwid)
+
+while(! zts.net_transport_is_ready(nwid)) {
+    zts.util_delay(50);
+}
 
 try {
-    console.log(zts.getNetworkIPv4Addr(nwid))
+    console.log(zts.addr_get_str(nwid, false))
 } catch (error) {
     console.log(error)
 }
 try {
-    console.log(zts.getNetworkIPv6Addr(nwid))
+    console.log(zts.addr_get_str(nwid, true))
 } catch (error) {
     console.log(error)
 }

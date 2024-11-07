497ad56dd735212c8ed9b20d79b7da213d8547f4
Grant Limberg <grant.limberg@zerotier.com>
2024-11-06 16:36:46 -0800

    Updating rust dependencies & bindgen/size_t fix

--- pkg/crate/libzt/build.rs
+++ pkg/crate/libzt/build.rs
@@ -5,15 +5,22 @@ use std::path::{Path, PathBuf};
 
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let lib_dir = Path::new(&dir).join("../../../dist/native/lib").canonicalize().unwrap();
+    let lib_dir = Path::new(&dir)
+        .join("../../../dist/native/lib")
+        .canonicalize()
+        .unwrap();
 
-    println!("cargo:rustc-link-search=native={}", lib_dir.to_string_lossy());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        lib_dir.to_string_lossy()
+    );
     println!("cargo:rustc-link-lib=static=zt");
     println!("cargo:rustc-link-lib=dylib=c++");
 
     let bindings = bindgen::Builder::default()
         .header("src/include/ZeroTierSockets.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .size_t_is_usize(true)
         .generate()
         .expect("Unable to generate bindings");
 
